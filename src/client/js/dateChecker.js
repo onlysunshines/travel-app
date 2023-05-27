aeb10d3e2315fb8971d488d7d3e178e7a29bbecc
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2023-05-27 14:20:50 -0600

    Add files via upload

---
+++ src/client/js/dateChecker.js
@@ -0,0 +1,14 @@
+// function checkDate() {
+//     let today = new Date();
+//     let trip = new Date(today.getFullYear(), `${document.getElementById("date")}`, `${document.getElementById("date")}`);
+//     if (today.getMonth() == `${document.getElementById("date")}` && today.getDate() > `${document.getElementById("date")}`) {
+//         trip.setFullYear(trip.getFullYear() + 1);
+//     }
+//     let one_day = 1000*60*60*24;
+
+//     console.log(Math.ceil((trip.getTime() - today.getTime()))/(one_day) + "days left until your Trip!");
+
+//     checkDate();
+// }
+
+// export { checkDate }
