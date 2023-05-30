b817c58cbcde2d66a366dc1adfcd83929fac0668
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2023-05-30 07:16:43 -0600

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -6,11 +6,6 @@ import './styles/resets.scss'
 import './styles/style.scss'
 import './styles/slideshow.scss'
 
-console.log(process.env.API_KEY_GEO);
-console.log(process.env.API_KEY_WEA);
-console.log(process.env.API_KEY_PIX);
-console.log(document.getElementById("inputPlace").value);
- 
 
 export {
     handleSubmit, checkDate, showSlides  
