a195874bb80e8c0b3a21f5a13c8f970226eaf6e3
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2023-06-01 07:36:25 -0600

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -4,14 +4,27 @@ let projectData = {};
 const path = require('path')
 const express = require('express')
 const cors = require("cors")
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 dotenv.config();
 
 const app = express()
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(express.static('dist'))
 
 app.use(cors())
+
+// const port = 3030;
+
+// const server = app.listen(port, listening);
+
+// function listening() {
+//     console.log('server running');
+//     console.log(`running on localhost: ${ port }`)
+// }
 
 console.log(__dirname)
 
@@ -19,7 +32,24 @@ app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
 
+app.post('/add', addData);
+
+function addData (req, res) {
+    newEntry = {
+        departure: req.body.days,
+        duration: req.body.tripLength,
+        current: req.body.currentTemp,
+        forecast: req.body.forecastArray,
+        historical: req.body.historyDailyArray,
+        photo: req.body.img
+    };
+
+    res.send(newEntry)
+}
+
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('travel-app listening on port 8080!')
 })
+
+console.log(`this is the project data ${projectData}`)
