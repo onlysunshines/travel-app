316ccd707968fdb6fb18ec7a0a852c2b1748bab2
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2023-06-01 07:34:20 -0600

    Update formHandler.spec.js

---
+++ src/__tests__/formHandler.spec.js
@@ -0,0 +1,9 @@
+import { formHandler } from "../client/js/formHandler";
+
+describe("Testing the submit functionality", () => {
+    test("Testing the formHandler() function", () => {
+        
+        expect( formHandler ).not.toBeNull();
+
+    });
+});
