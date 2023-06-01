b89d5e2b561c212ad811ac9815883e44266e0f37
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2023-06-01 07:36:01 -0600

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,10 @@
 import { handleSubmit } from './js/formHandler'
-import { checkDate } from './js/dateChecker';
-import { showSlides } from './js/slideShow';
+import { showSlides } from './js/slideShow'
 
 import './styles/resets.scss'
 import './styles/style.scss'
 import './styles/slideshow.scss'
 
-
 export {
-    handleSubmit, checkDate, showSlides  
+    handleSubmit, showSlides
 }
